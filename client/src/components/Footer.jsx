d79c1e2fab7786a7191efcd34a3988ed47f9000c
phudthw <vanphu869@gmail.com>
2024-06-12 04:41:23 -0900

    create the footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,66 @@
+import { Footer } from "flowbite-react";
+import { Link } from "react-router-dom";
+import { BsFacebook, BsInstagram, BsGithub, BsDiscord, BsTwitter } from 'react-icons/bs'
+
+export default function FooterCom() {
+    return (
+        <Footer container className="border border-t-8 border-pink-500">
+            <div className="w-full max-w-7xl mx-auto">
+                <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+                    <div className="mt-5">
+                    <Link to="/" className='selt-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'>
+                        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>DevTeam's</span>
+                        FullStack
+                    </Link>
+                    </div>
+                    <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
+                        <div>
+                            <Footer.Title title="About" />
+                            <Footer.LinkGroup col>
+                                <Footer.Link href="" target="_blank" rel="noopener noreferrer">
+                                    Demo project
+                                </Footer.Link>
+                                <Footer.Link href="/about" target="_blank" rel="noopener noreferrer">
+                                    About
+                                </Footer.Link>
+                            </Footer.LinkGroup>
+                        </div>
+                        <div>
+                            <Footer.Title title="Follow Us" />
+                            <Footer.LinkGroup col>
+                                <Footer.Link href="https://github.com/phudthw/" target="_blank" rel="noopener noreferrer">
+                                    Github
+                                </Footer.Link>
+                                <Footer.Link href="#">
+                                    Discord
+                                </Footer.Link>
+                            </Footer.LinkGroup>
+                        </div>
+                        <div>
+                            <Footer.Title title="Legal" />
+                            <Footer.LinkGroup col>
+                                <Footer.Link href="#">
+                                    Privacy Policy
+                                </Footer.Link>
+                                <Footer.Link href="#">
+                                    Terms &amp; Conditions
+                                </Footer.Link>
+                            </Footer.LinkGroup>
+                        </div>
+                    </div>
+                </div>
+                <Footer.Divider />
+                <div className="w-full sm:flex sm:items-center sm:justify-between">
+                    <Footer.Copyright href="#" by="Teams Dev 🎁" year={new Date().getFullYear()}/>
+                    <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center md:mt-3">
+                        <Footer.Icon href="https://facebook.com/phudthw/" icon={BsFacebook} />
+                        <Footer.Icon href="#" icon={BsInstagram} />
+                        <Footer.Icon href="https://github.com/phudthw/" icon={BsGithub} />
+                        <Footer.Icon href="#" icon={BsDiscord} />
+                        <Footer.Icon href="#" icon={BsTwitter} />
+                    </div>
+                </div>
+            </div>
+        </Footer>
+    )
+}
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,8 +10,8 @@ export default function Header() {
   return (
     <Navbar className='border-b-2'>
         <Link to="/" className='selt-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
-            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Sahand's</span>
-            BLOG
+            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>DevTeam</span>
+            FullStack
         </Link>
         <form>
             <TextInput 
