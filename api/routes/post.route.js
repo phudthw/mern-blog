0b18f54311b79e186debb91339dbeadbcf89026c
phudthw <vanphu869@gmail.com>
2024-07-13 11:00:31 -0900

    đã sửa và thêm phần update

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { verifyToken } from '../utils/verifyuser.js'
-import { create, deletepost, getposts } from "../controllers/post.controller.js";
+import { create, deletepost, getposts, updatepost } from "../controllers/post.controller.js";
 
 const route = express.Router()
 
 route.post('/create', verifyToken, create)
 route.get('/getposts', getposts)
 route.delete('/deletepost/:postId/:userId', verifyToken, deletepost)
+route.put('/updatepost/:postId/:userId', verifyToken, updatepost)
 
 export default route
